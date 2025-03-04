81257b22ba997cfb190c9fc1d571716da9723949
musmuschio <musmuschiohaa@gmail.com>
2025-03-05 01:13:14 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ self.addEventListener("notificationclick", event => {
         clients.openWindow("https://github.com") // Ganti dengan link tujuan
     );
 });
+self.addEventListener("install", function(event) {
+  console.log("Service Worker Terinstall");
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log("Fetching:", event.request.url);
+});
